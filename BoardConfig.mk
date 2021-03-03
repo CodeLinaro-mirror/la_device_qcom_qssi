a70301005b5ed3dba3710a8b44a090ca064394b1
YSR <suresh@codeaurora.org>
2021-03-03 13:31:06 -0800

    vsdk:Enable Configurable Ramdisk snapshot.
    Enabled Ramdisk snapshot configuration file and
    that will be updated to generate ramdisk snapshot
    
    Change-Id: I74b3d5137f22e78b20c58a4b9227b1fd32dcba24

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -133,6 +133,7 @@ BUILD_BROKEN_USES_BUILD_HOST_STATIC_LIBRARY := true
 #Enable VNDK Compliance
 BOARD_VNDK_VERSION:=current
 RECOVERY_SNAPSHOT_VERSION := current
+RAMDISK_SNAPSHOT_VERSION := current
 Q_BU_DISABLE_MODULE := true
 
 ###### Dynamic Partition Handling ####
@@ -172,5 +173,8 @@ include device/qcom/sepolicy/SEPolicy.mk
 
 DIRECTED_VENDOR_SNAPSHOT := true
 DIRECTED_RECOVERY_SNAPSHOT := true
+DIRECTED_RAMDISK_SNAPSHOT := true
+
 include device/qcom/qssi/vendor_snapshot_modules.mk
 include device/qcom/qssi/recovery_snapshot_modules.mk
+include device/qcom/qssi/ramdisk_snapshot_modules.mk
