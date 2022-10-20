d73c0742c30cdb6524089d216bf12984f042aff6
Mittadodla Penchala Prasad <quic_mpenchal@quicinc.com>
2022-10-20 15:01:35 +0530

    qssi: Automotive enable on qssi
    Automotive enable on qssi
    
    Change-Id: Ic41466afb58f7914b2403f21a31e9b6ac8240494

--- common64.mk
+++ common64.mk
@@ -6,11 +6,14 @@ $(call inherit-product, device/qcom/qssi/base.mk)
 $(call inherit-product-if-exists, $(QCPATH)/common/config/device-vendor-qssi.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, packages/services/Car/car_product/build/car.mk)
 
 PRODUCT_BRAND := qcom
 PRODUCT_AAPT_CONFIG += hdpi mdpi
 
 PRODUCT_MANUFACTURER := QUALCOMM
+
+BOARD_IS_AUTOMOTIVE := true
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.backup.ntpServer=0.pool.ntp.org \
