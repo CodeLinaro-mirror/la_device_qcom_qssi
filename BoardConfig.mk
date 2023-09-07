8df76fcc3edfa7969a34705dc613bf14332e6273
Sudheer Kanchumurth <c_skanch@qti.qualcomm.com>
2023-09-07 10:41:51 -0700

    qssi: Generate system_dlkm symlink
    add flag for generating system_dlkm symlink to
    BoardConfig.mk file.
    
    add generation of system_dlkm symlink in new
    symlink_system_dlkm.mk file
    
    Change-Id: I7832a1e979fec6f5f88632793f64aefda1d6e1c9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -30,6 +30,9 @@ TARGET_NO_KERNEL := true
 # Disable DLKMs compilation for lunch qssi builds.
 TARGET_KERNEL_DLKM_DISABLE := true
 
+# create symlink system/lib/modules -> system_dlkm/lib/modules
+CREATE_SYSTEM_DLKM_SYMLINK := true
+
 -include $(QCPATH)/common/msmnile/BoardConfigVendor.mk
 
 USE_OPENGL_RENDERER := true
