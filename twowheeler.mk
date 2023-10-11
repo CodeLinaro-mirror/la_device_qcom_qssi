22d7480c18b26b36630b60c47023d2655200f94a
Arun Raj Shanmugam <quic_arunshan@quicinc.com>
2023-10-11 19:26:04 +0530

    device-qcom-qssi: Remove LOW RAM changes
    Remove Go configs to disable LOW RAM changes.
    
    Change-Id: Iee2ccfcb452ddf4a0697abd5695d41edd82ba393
    Signed-off-by: Arun Raj Shanmugam <quic_arunshan@quicinc.com>

--- twowheeler.mk
+++ twowheeler.mk
@@ -18,22 +18,3 @@ ifneq ($(USE_CAR_SVCS_FOR_2W),true)
 PRODUCT_COPY_FILES += \
     device/qcom/qssi/android.hardware.vehicle.bike.xml:system/etc/permissions/android.hardware.vehicle.bike.xml
 endif
-
-# Reference qssi_32go
-##############################Go configs###########################################
-#Go variant flag
-TARGET_HAS_LOW_RAM := true
-
-# Enable DM file preopting to reduce first boot time
-PRODUCT_DEX_PREOPT_GENERATE_DM_FILES := true
-PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
-
-DONT_UNCOMPRESS_PRIV_APPS_DEXS := true
-
-$(call inherit-product, build/target/product/go_defaults.mk)
-
-# Disable surfaceflinger prime_shader cache to improve post boot memory.
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += service.sf.prime_shader_cache=0
-
-#########################End of Go configs########################################
-
