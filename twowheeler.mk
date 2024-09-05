6cac9abb755c4bfbcc5e02aa0da3497ff0397b65
Rinkey Kumari <quic_rinkkuma@quicinc.com>
2024-09-05 17:04:44 +0530

    Adding sys property
    - Settings sys property to control
      cache kill process on boot
    
    Change-Id: If818a4ca97308f8dd46092f563e0924185c69891
    Signed-off-by: Rinkey Kumari <quic_rinkkuma@quicinc.com>

--- twowheeler.mk
+++ twowheeler.mk
@@ -6,7 +6,8 @@ PRODUCT_SYSTEM_PROPERTIES += \
 
 #system prop for Hardware type Automotive
 PRODUCT_SYSTEM_PROPERTIES += \
-    ro.hardware.type=automotive
+    ro.hardware.type=automotive \
+    sys.no_kill_cached_proc_post_boot_completed_duration_millis=0
 
 PRODUCT_COPY_FILES += \
     device/qcom/qssi/android.hardware.vehicle.bike.xml:system/etc/permissions/android.hardware.vehicle.bike.xml
